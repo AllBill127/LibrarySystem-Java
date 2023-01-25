70411a1eccf92e7272d256ee5a4a802c36a09eeb
Aleksandras Bilevičius <90201218+AllBill127@users.noreply.github.com>
2023-01-25 15:20:07 +0200

    Initial documentation upload

---
+++ documentation/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"myExceptions"},{"l":"myInterface"},{"l":"myLiterature"},{"l":"myPlanFactory"},{"l":"myUI"},{"l":"myUser"},{"l":"myUtility"}];updateSearchResults();
